244ebc252492b86a53b502f0b488f6a3de957c63
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-03-16 21:33:49 -0700

    Enhance Position class with private properties and add update, move, and get methods for better encapsulation and functionality

--- src/position.ts
+++ src/position.ts
@@ -1,9 +1,23 @@
 export class Position {
-    x: number
-    y: number
+    private x: number
+    private y: number
 
     constructor(x: number = 0, y: number = 0) {
         this.x = x
         this.y = y
+    }
+
+    update(x: number, y: number) {
+        this.x = x
+        this.y = y
+    }
+
+    move(x: number, y: number) {
+        this.x += x
+        this.y += y
+    }
+
+    get() {
+        return { x: this.x, y: this.y }
     }
 }
