45f53fe3ef1ed4c0a8f503b57021817be90290b6
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-03-16 21:53:26 -0700

    Refactor Position class to use public properties and simplify move method

--- src/position.ts
+++ src/position.ts
@@ -1,30 +1,14 @@
 export class Position {
-    private _x: number
-    private _y: number
+    x: number
+    y: number
 
     constructor({ x, y }: { x: number; y: number }) {
-        this._x = x
-        this._y = y
+        this.x = x
+        this.y = y
     }
 
-    get x(): number {
-        return this._x
-    }
-
-    set x(value: number) {
-        this._x = value
-    }
-
-    get y(): number {
-        return this._y
-    }
-
-    set y(value: number) {
-        this._y = value
-    }
-
-    move({ x = 0, y = 0 }: { x?: number; y?: number }) {
-        this._x += x
-        this._y += y
+    move({ dx, dy }: { dx: number; dy: number }) {
+        this.x += dx
+        this.y += dy
     }
 }
