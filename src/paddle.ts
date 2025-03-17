d781673a7ead5ff64ac46f18d2766898e1f653d6
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-03-16 22:45:14 -0700

    Refactor Paddle class to encapsulate speed property and add methods for speed adjustment

--- src/paddle.ts
+++ src/paddle.ts
@@ -4,12 +4,24 @@ export class Paddle {
     position: Point
     width: number
     height: number
-    speed: number
+    private _speed: number
 
     constructor(position: Point, width: number, height: number, speed: number) {
         this.position = position
         this.width = width
         this.height = height
-        this.speed = speed
+        this._speed = speed
+    }
+
+    get speed() {
+        return this._speed
+    }
+
+    increaseSpeed() {
+        this._speed += 0.2
+    }
+
+    decreaseSpeed() {
+        this._speed -= 0.2
     }
 }
