f91f850a82cfe463ce348116eab87fd9edb01c1e
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-03-16 22:25:10 -0700

    Update Paddle constructor to use Coordinates type for improved type safety

--- src/paddle.ts
+++ src/paddle.ts
@@ -1,4 +1,5 @@
 import { Position } from './position'
+import { Coordinates } from './types'
 
 export class Paddle {
     position: Position
@@ -6,7 +7,7 @@ export class Paddle {
     height: number
     speed: number
 
-    constructor(coordinates: { x: number; y: number }, width: number, height: number, speed: number) {
+    constructor(coordinates: Coordinates, width: number, height: number, speed: number) {
         this.position = new Position(coordinates)
         this.width = width
         this.height = height
