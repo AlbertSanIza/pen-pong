06a72fccefed92d59894ec001adedc0618c88806
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-03-16 21:40:05 -0700

    Refactor Position class constructor to improve property initialization

--- src/position.ts
+++ src/position.ts
@@ -2,17 +2,16 @@ export class Position {
     private _x: number
     private _y: number
 
-    get x(): number {
-        return this._x
-    }
-
-    get y(): number {
-        return this._y
-    }
-
     constructor(x: number = 0, y: number = 0) {
         this._x = x
         this._y = y
+    }
+
+    get x(): number {
+        return this._x
+    }
+    get y(): number {
+        return this._y
     }
 
     update(x: number, y: number) {
